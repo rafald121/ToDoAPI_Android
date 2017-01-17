bc7092f7a153421494b9e2fd745565371b610bbb
rafald121 <rafald121@gmail.com>
2017-01-17 15:01:25 +0100

    add: activity_login and LoginActivity.java

---
+++ app/src/main/java/com/example/android/todoapi_android/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.todoapi_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rafaello on 2017-01-17.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
+
