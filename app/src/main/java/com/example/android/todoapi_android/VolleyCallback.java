8d54a272630b85959fa358dbc03462ba9147cfe0
rafald121 <rafald121@gmail.com>
2017-01-26 18:53:44 +0100

    WHAT

---
+++ app/src/main/java/com/example/android/todoapi_android/VolleyCallback.java
@@ -0,0 +1,11 @@
+package com.example.android.todoapi_android;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Rafaello on 2017-01-26.
+ */
+
+public interface VolleyCallback{
+    void onSuccess(String result);
+}
